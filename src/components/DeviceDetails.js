92c9a37a30de12f5e87259b6b9c0b071e49f0ca7
rcarman09 <71738795+rcarman09@users.noreply.github.com>
2023-09-20 00:04:58 -0400

    Using this branch as a testing environment for the frontend

---
+++ src/components/DeviceDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DeviceDetails() {
+  // TODO: Fetch details of the selected device from the API
+
+  return (
+    <div>
+      {/* Render device details here */}
+    </div>
+  );
+}
+
+export default DeviceDetails;
---
+++ src/components/SettingsPanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SettingsPanel() {
+  // TODO: Implement settings configuration
+
+  return (
+    <div>
+      {/* Render settings configuration form here */}
+    </div>
+  );
+}
+
+export default SettingsPanel;
