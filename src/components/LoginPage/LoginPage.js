8e5ae03571919d60a456f63ce1d7238d882ac229
rcarman09 <71738795+rcarman09@users.noreply.github.com>
2023-10-20 20:02:37 -0400

    Improved user authentication UI with Material-UI components
    Description:
    
    This commit changes the user interface for the registration and login pages, using Material-UI components to improve the aesthetic and user experience.
    
    Changes include:
    - Replaced native HTML elements with Material-UI components in both the registration and login forms, ensuring stylistic consistency across these user authentication pages.
    - Applied custom styling via the 'sx' prop to adjust layout, spacing, and color schemes, aligning the design with modern UI trends.
    - Enhanced user guidance by adding clearer calls-to-action, including a 'Sign In' link on the registration page for users who already have an account, and vice versa.
    - Removed external CSS dependencies, streamlining style adjustments within components and enhancing the maintainability of the codebase.
    
    These changes improve the user experience when logging in/registering on our website.

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,34 +1,91 @@
-
 import React, { useState } from 'react';
-import './LoginPage.css';  // Importing the styles from App.css
-import { Link } from 'react-router-dom';
+import { Button, TextField, Box, Typography, Container, Link } from '@mui/material';
 
+const LoginPage = () => {
+  const [credentials, setCredentials] = useState({ username: '', password: '' });
 
-function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
   };
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission, e.g., call an authentication API.
+    console.log(credentials);
   };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <label htmlFor="username" className="login-label">Username</label>
-      <input type="text" id="username" name="username" value={username} onChange={handleUsernameChange} className="login-input" />
-      <label htmlFor="password" className="login-label">Password</label>
-      <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange} className="login-input" />
-      <input type="submit" value="Submit" className="login-submit" />
-      <p>Don't have an account? <Link to="/register">Register Here!</Link></p>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          // This will add a contrasting background, padding, and a border radius
+          bgcolor: 'background.paper',
+          boxShadow: 1,
+          borderRadius: 1,
+          p: 3,  // padding
+          mt: 8,  // margin-top
+          mb: 4,  // margin-bottom
+        }}
+      >
+        <Typography 
+        component="h1" 
+        variant="h5"
+        sx={{ color: 'text.primary' }}  // This represents black or a very dark color in a standard theme
+        >
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, width: '100%' }}> {/* Full width form */}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={credentials.username}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={credentials.password}
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          {/* Adding a link to navigate to the registration page */}
+          <Link href="/register" variant="body2">
+            {"Create an Account"}
+          </Link>
+        </Box>
+      </Box>
+    </Container>
   );
-}
+};
 
 export default LoginPage;
+
+
 
     
--- src/components/RegistrationPage/RegistrationPage.js
+++ src/components/RegistrationPage/RegistrationPage.js
@@ -1,31 +1,108 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './RegistrationPage.css'; // Make sure this path is correct
+import React, { useState } from 'react';
+import { Button, TextField, Box, Typography, Container, Link } from '@mui/material';
 
-function RegistrationPage() {
+const RegistrationPage = () => {
+  const [credentials, setCredentials] = useState({ username: '', email: '', password: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission, e.g., call an authentication API.
+    console.log(credentials);
+  };
+
   return (
-    <div className="registration-container"> {/* Adjusted class name */}
-      <form className="registration-form"> {/* Adjusted class name */}
-        <h2>Register</h2> {/* Moved inside the form for consistency */}
-        
-        <label>Create a Username:</label>
-        <input type="text" name="username" className="registration-input" /> {/* Adjusted class name */}
-        
-        <label>Create a Password:</label>
-        <input type="password" name="password" className="registration-input" /> {/* Adjusted class name */}
-        
-        <label>Confirm Password:</label>
-        <input type="password" name="confirmPassword" className="registration-input" /> {/* Adjusted class name */}
-        
-        <button type="submit" className="registration-button">Register Now!</button> {/* Adjusted class name */}
-      </form>
-      <p>Already have an account? 
-        <Link to="/login" className="registration-link">Login Here!</Link> {/* Removed inline styles and adjusted class name */}
-      </p>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          bgcolor: 'background.paper',
+          boxShadow: 1,
+          borderRadius: 1,
+          p: 3,
+          mt: 8,
+          mb: 4,
+        }}
+      >
+        <Typography component="h1" variant="h5" sx={{ color: 'text.primary' }}>
+          Sign Up
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, width: '100%' }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={credentials.username}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={credentials.email}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="new-password"
+            value={credentials.password}
+            onChange={handleChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Box 
+          component="div" 
+          sx={{ 
+            width: '100%',  // Take up the full width of the form
+            display: 'flex', 
+            justifyContent: 'center',  // Center the content horizontally
+            mt: 1  // Optional: adds some spacing above the text
+          }}
+        >
+          <Typography component="p" variant="body2" sx={{ color: 'text.primary', mt: 1 }}>
+          Already have an account?&nbsp;
+          <Link href="/login" variant="body2">
+            Sign In
+          </Link>
+          </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
   );
-}
+};
 
 export default RegistrationPage;
+
 
 
