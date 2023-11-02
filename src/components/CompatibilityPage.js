962b9bdc4587138f6b272d93cf12c1ccc3f5c5f1
huthbn <huthbn@mail.uc.edu>
2023-11-02 09:55:36 -0400

    Added text to Compatibility Page

--- src/components/CompatibilityPage.js
+++ src/components/CompatibilityPage.js
@@ -5,7 +5,14 @@ function CompatibilityPage() {
   return (
     <div>
       <h1 style={{fontSize: '2em', fontWeight: 'bold', paddingTop: '80px', textAlign: 'center'}}>Compatibility</h1>
-      <p>This is the Compatibility Page.</p>
+      <h2> Compatible Devices for Registration on Stay Aware: </h2>
+
+        <dl>
+          <dt> LIFX Mini 800-Lumen LED Light Bulb </dt>
+            <dd>- Smart lightbulb that can be controlled from the cloud on iOS, Android, and PC! </dd>
+          <dt> Rasberry Pi 4 </dt>
+            <dd> Pocket-sized computer that can also function as a smart hub, media centre, and more! </dd>
+        </dl>
     </div>
   );
 }
