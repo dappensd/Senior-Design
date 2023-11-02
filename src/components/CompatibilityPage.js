411215024eb128de755393b7cb8dbf818d4fb2f5
huthbn <huthbn@mail.uc.edu>
2023-11-02 13:08:37 -0400

    Updates to Compatibility Page

--- src/components/CompatibilityPage.js
+++ src/components/CompatibilityPage.js
@@ -1,17 +1,23 @@
 
 import React from 'react';
+import './CompatibilityPage.css';
 
 function CompatibilityPage() {
   return (
     <div>
       <h1 style={{fontSize: '2em', fontWeight: 'bold', paddingTop: '80px', textAlign: 'center'}}>Compatibility</h1>
-      <h2> Compatible Devices for Registration on Stay Aware: </h2>
-
+      <h2> Stay Aware Compatibile Devices: </h2>
         <dl>
-          <dt> LIFX Mini 800-Lumen LED Light Bulb </dt>
+          <dt> LIFX Candle Color Wi-Fi Smart LED Multicolors Light Bulb </dt>
             <dd>- Smart lightbulb that can be controlled from the cloud on iOS, Android, and PC! </dd>
+          <div className="link">
+            <dd> Check out the lightbulb <a href = 'https://www.amazon.com/LIFX-E12-Candle-Color-Wi-Fi/dp/B07XKYKJ9Q' target="_blank" rel="noreferrer"> here!</a> </dd>
+          </div>
           <dt> Rasberry Pi 4 </dt>
-            <dd> Pocket-sized computer that can also function as a smart hub, media centre, and more! </dd>
+            <dd>- Pocket-sized computer that can also function as a smart hub, media centre, and more! </dd>
+          <div className = "link">
+            <dd> Check out one of the models <a href = 'https://www.amazon.com/Raspberry-Pi-Computer-Suitable-Workstation/dp/B0899VXM8F/ref=sr_1_4?crid=16QK1HD08Y70V&keywords=raspberry+pi+4&qid=1698936765&sprefix=rasberrypi+4%2Caps%2C127&sr=8-4' target="_blank" rel="noreferrer"> here! </a> </dd>
+          </div>
         </dl>
     </div>
   );
