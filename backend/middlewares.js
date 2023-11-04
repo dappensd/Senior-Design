20392f345ed4b0845da018c9ec787c4b951eb6c1
rcarman09 <71738795+rcarman09@users.noreply.github.com>
2023-11-03 21:00:31 -0400

    New Packages installed for Backend

---
+++ backend/middlewares.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+// JWT Authentication Middleware
+const jwtMiddleware = (req, res, next) => {
+    const token = req.header('Authorization');
+
+    if (!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
+};
+
+module.exports = {
+    jwtMiddleware: jwtMiddleware
+};
