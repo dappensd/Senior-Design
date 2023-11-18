fd576a67b32d88a9df2d754d89f5bd31bb402fde
rcarman09 <71738795+rcarman09@users.noreply.github.com>
2023-11-18 17:56:09 -0500

    Creating tsparticles Background

--- src/components/ParticlesBackground.js
+++ src/components/ParticlesBackground.js
@@ -1,32 +1,26 @@
-// ParticlesBackground.js
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Particles from 'react-tsparticles';
-import particlesConfig from '.././particlesConfig'; // Import your particles configuration
+import { loadSlim } from 'tsparticles-slim';
+import particlesConfig from '.././particlesConfig'; // Adjust the path as needed
 
 const ParticlesBackground = () => {
-  // Removed the zIndex from particlesStyle since we're setting it in the <style> tag below
-  const particlesStyle = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    opacity: 1
-  };
+  const options = useMemo(() => {
+    return {
+      ...particlesConfig, // Use your existing configuration
+      fullScreen: {
+        enable: true,
+        zIndex: -1 // Set the z-index to -1
+      }
+    };
+  }, []);
 
-  return (
-    <>
-      <style>
-        {`
-          #tsparticles > canvas {
-            z-index: -1 !important;
-          }
-        `}
-      </style>
-      <Particles id="tsparticles" style={particlesStyle} params={particlesConfig} />
-    </>
-  );
+  const particlesInit = useCallback((engine) => {
+    loadSlim(engine);
+  }, []);
+
+  return <Particles init={particlesInit} options={options} />;
 };
 
 export default ParticlesBackground;
+
 
