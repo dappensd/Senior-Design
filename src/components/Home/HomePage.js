0b391809c4344ab5a2ca0bd1d9f19e66f9e3e7cb
huthbn <142831406+huthbn@users.noreply.github.com>
2024-01-19 13:36:42 -0500

    Added arrows to slick-carousel

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -4,6 +4,18 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+function SlideArrows(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "black" }}
+      onClick={onClick}
+    />
+  );
+}
+
+
 function HomePage() {
   var settings = {
     dots: true,
@@ -11,6 +23,8 @@ function HomePage() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    prevArrow: <SlideArrows />,
+    nextArrow: <SlideArrows />
   };
 
   return (
