5a4edc123bf1daba0e37a56e2ca8c1cc24f53e93
huthbn <142831406+huthbn@users.noreply.github.com>
2024-02-19 21:29:07 -0500

    Updated Documentation page for LIFX Bulb

--- src/components/Documentation/DeviceManagement.js
+++ src/components/Documentation/DeviceManagement.js
@@ -10,13 +10,35 @@ function DeviceManagement() {
                 </div>
 
                 <div className={styles.content}>
-                    <h2> To register your device, follow these steps:</h2>
-                        <ol>
-                            <li> Click on the Devices page in the navigation </li>
-                            <li> Click "Add New Device" </li>
-                            <li> Type in the Device ID and click "Register Device" </li>
-                        </ol> 
-                </div>
+                    <h2> Adding an LIFX Device </h2>
+                        <h3> Requirements: </h3>
+                            <ul>
+                                <li> An LIFX Account  </li>
+                                <li> Device Name </li>
+                                <li> LIFX Device </li>
+                            </ul>
+                        <div>
+                            <h4> Instructions: </h4>
+                                <ol>
+                                    <li> Create an LIFX account at <a href="https://cloud.lifx.com" target="_blank" rel="noreferrer">https//cloud.lifx.com</a></li>
+                                    <li> Click on your account in the upper-right corner of the screen, then select <b> Personal Access Tokens</b></li>
+                                    <li> Select <b> Generate New Token</b>. You will be given a string of numbers and letters</li>
+                                        <ul>
+                                            <li><b> Note: </b> You will need to save this token as you will <b> NOT </b> be able to view the token again once you leave the page</li>
+                                            <li> You can choose to generate another token if you forget it by selecting the <b> Generate New Token</b> </li>  
+                                        </ul>
+                                    <li> Once you have the token, select <b> Login </b>, then select <b> Sign Up.</b> </li>
+                                        <ul>
+                                            <li> Enter a username in the <b> Username</b> field.</li>
+                                            <li> Enter an email address in the <b> Email Address </b> field.</li>
+                                            <li> Enter a unique and strong password in the <b>Password</b> field.</li>
+                                            <li> Select the <b> Sign Up </b> to finish creating the account.</li>
+                                        </ul>
+                                    <li> Once you hav an account created, select the <b> Documentation</b> page in the nav bar, then select <b> Add New Device</b> </li>
+                                    <li> Enter a unique name for the <b>Device ID</b> field and the token in the <b>Token</b> field.</li>
+                                </ol>
+                        </div>
+                </div>  
             </div>
         </div>
 
